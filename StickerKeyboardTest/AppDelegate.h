bc50c81e36b063880f14782a650aa5ff246ece7b
Ostap Horbach <phnx@Ostaps-MacBook-Pro.local>
2015-06-21 00:51:54 +0300

    Initial Commit

---
+++ StickerKeyboardTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StickerKeyboardTest
+//
+//  Created by Ostap Horbach on 6/21/15.
+//  Copyright (c) 2015 Ostap Horbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StickerKeyboardTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StickerKeyboardTest
+//
+//  Created by Ostap Horbach on 6/21/15.
+//  Copyright (c) 2015 Ostap Horbach. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
